bfa0852d4fb142fc2f65d5f42d3c9388b152c27e
Saïd VELO <saidali.velo@gmail.com>
2020-10-29 15:33:50 +0100

    header in progress

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,19 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  path = '../../assets/socials-icons/'
+  socialList = [
+    {alt: 'Logo Gmail', src: this.path + 'email.png'},
+    {alt: 'Logo Facebook', src: this.path + 'facebook.png'},
+    {alt: 'Logo Twitter', src: this.path + 'twitter.png'},
+    {alt: 'Logo Instagram', src: this.path + 'instagram.png'},
+    {alt: 'Logo Youtube', src: this.path + 'youtube.png'},
+    {alt: 'Logo Spotify', src: this.path + 'facebook.png'},
+  ]
   constructor() { 
   }
 
   ngOnInit(): void {
-    const path = '../../assets/socials-icons/'
-    const socialList = [
-      {alt: 'Logo Gmail', src: path + 'email.png'},
-      {alt: 'Logo Facebook', src: path + 'facebook.png'},
-      {alt: 'Logo Twitter', src: path + 'twirrer.png'},
-      {alt: 'Logo Instagram', src: path + 'instagram.png'},
-      {alt: 'Logo Youtube', src: path + 'youtube.png'},
-      {alt: 'Logo Spotify', src: path + 'facebook.png'},
-    ]
+
   }
 
 }
